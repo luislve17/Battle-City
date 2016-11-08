61403657649e52ecc0d09dba477e0567ca13e36e
Luis Vasquez <luislve1796@gmail.com>
2016-11-08 14:21:56 -0500

    Create test1.c

---
+++ Game/test1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "tools/InputX.h"
+#include "tools/AnimX.h"
+#include "tools/MotorX.h"
+#include "tools/NivelesX.h"
+
+int main(){
+	invocMenu();
+	return 0;
+}
