6a9b3410388ef36fae37c5ccb0f501d3e0ab1caf
Luis Vasquez <luislve1796@gmail.com>
2016-11-08 14:22:48 -0500

    Create AnimX.h

---
+++ Game/tools/AnimX.h
@@ -0,0 +1,11 @@
+#ifndef ANIMX_H
+#define ANIMX_H
+
+void invocMenu();
+void limpOut(int backsteps);
+void minisleep();
+void genAnimMenu(int frameFlag1, int frameFlag2);
+void impFotograma(int i);
+int menuClave();
+
+#endif
